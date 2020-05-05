2dc9a1219c479e8227bbf6890f955ac2e6536fc2
AMereBagatelle <themerebagatelle@gmail.com>
2020-05-05 15:19:33 -0400

    Renaming MessageListener.java to ServerMessageListener.java

--- src/main/java/amerebagatelle/github/io/discordconnect/bot/Bot.java
+++ src/main/java/amerebagatelle/github/io/discordconnect/bot/Bot.java
@@ -1,14 +1,8 @@
 package amerebagatelle.github.io.discordconnect.bot;
 
-import amerebagatelle.github.io.discordconnect.settings.SettingsManager;
-import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.TextChannel;
-import net.dv8tion.jda.api.events.ReadyEvent;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.text.LiteralText;
@@ -30,7 +24,7 @@ public class Bot {
     public Bot() {
         try {
             bot = JDABuilder.createDefault("Njk2NTE0ODkwMzYxMzM5OTM0.XrFC4A.DXeBIIuGsq4w0bY785e3fEM24Lc").build();
-            bot.addEventListener(new MessageListener());
+            bot.addEventListener(new ServerMessageListener());
             bot.awaitReady();
             linkChannel = bot.getTextChannelById(linkChannelId);
             isBotActive = true;
--- src/main/java/amerebagatelle/github/io/discordconnect/bot/ServerMessageListener.java
+++ src/main/java/amerebagatelle/github/io/discordconnect/bot/ServerMessageListener.java
@@ -7,15 +7,15 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.annotation.Nonnull;
 
-public class MessageListener extends ListenerAdapter {
+public class ServerMessageListener extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(@Nonnull MessageReceivedEvent event) {
         Message message = event.getMessage();
         String content = message.getContentRaw();
 
-        if(message.getChannel() == DiscordConnect.bot.linkChannel) {
-            if(!content.startsWith(DiscordConnect.bot.minecraftMessagePrefix)) {
+        if (message.getChannel() == DiscordConnect.bot.linkChannel) {
+            if (!content.startsWith(DiscordConnect.bot.minecraftMessagePrefix)) {
                 DiscordConnect.bot.sendMessageToMinecraft(DiscordConnect.bot.minecraftMessagePrefix + " " + content);
             }
         }
