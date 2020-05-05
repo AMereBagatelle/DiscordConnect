c2590d36016d30716fe1f4ff2672352d55f068b0
AMereBagatelle <themerebagatelle@gmail.com>
2020-05-05 15:12:14 -0400

    Finalizing changes to MessageListener

--- src/main/java/amerebagatelle/github/io/discordconnect/bot/MessageListener.java
+++ src/main/java/amerebagatelle/github/io/discordconnect/bot/MessageListener.java
@@ -3,7 +3,6 @@ package amerebagatelle.github.io.discordconnect.bot;
 import amerebagatelle.github.io.discordconnect.DiscordConnect;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.annotation.Nonnull;
@@ -22,8 +21,5 @@ public class MessageListener extends ListenerAdapter {
                 DiscordConnect.bot.sendMessageToMinecraft(DiscordConnect.bot.minecraftMessagePrefix + " " + content);
             }
         }
-
-        // Makes sure that we don't catch our own message
-        if (event.getAuthor().isBot()) return;
     }
 }
