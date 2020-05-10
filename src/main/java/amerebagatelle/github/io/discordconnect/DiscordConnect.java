250631b36b8e31469d6d74ad7ec75f7900629803
AMereBagatelle <themerebagatelle@gmail.com>
2020-05-10 16:17:35 -0400

    Adding server start and stop messages

--- src/main/java/amerebagatelle/github/io/discordconnect/DiscordConnect.java
+++ src/main/java/amerebagatelle/github/io/discordconnect/DiscordConnect.java
@@ -3,6 +3,8 @@ package amerebagatelle.github.io.discordconnect;
 import amerebagatelle.github.io.discordconnect.bot.Bot;
 import amerebagatelle.github.io.discordconnect.settings.SettingsManager;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.server.ServerStartCallback;
+import net.fabricmc.fabric.api.event.server.ServerStopCallback;
 
 public class DiscordConnect implements ModInitializer {
     public static Bot bot;
@@ -11,5 +13,13 @@ public class DiscordConnect implements ModInitializer {
     public void onInitialize() {
         SettingsManager.initSettings();
         bot = new Bot();
+
+        ServerStartCallback.EVENT.register(e -> {
+            bot.sendMessageToChatlink("Server started!");
+        });
+
+        ServerStopCallback.EVENT.register(e -> {
+            bot.sendMessageToChatlink("Server shutting down...");
+        });
     }
 }
